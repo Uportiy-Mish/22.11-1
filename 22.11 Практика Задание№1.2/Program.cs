f0d886a66eef19f626222482c8a147e418ec9880
Asus <Asus@DESKTOP-Q1VVDVK>
2023-11-22 11:58:50 +0300

    Добавьте файлы проекта.

---
+++ 22.11 Практика Задание№1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _22._11_Практика_Задание_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1;
+            name1 = Interaction.InputBox("Введите ваше имя",
+                "Ввод имени");
+            string name2;
+            name2 = Interaction.InputBox("Введите вашу фамилию",
+                "Ввод фамилии");
+            string txt = "Имя: " + name1 + ", Фамилия: " + name2;
+            MessageBox.Show(txt, "Итог.");
+
+        }
+    }
+}
---
+++ 22.11 Практика Задание№1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._11_Практика_Задание_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Имя.");
+            string name1 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Введите Фамилию.");
+            string name2 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Имя: {name1}, Фамилия: {name2}");
+            Console.ReadKey();
+
+        }
+    }
+}
